967c39c52ac79a3d47b3d3bb197a3f3fdc1c7a75
sadmandmcx <radio.android67@gmail.com>
2019-09-19 00:05:37 -0700

    initial commit

---
+++ content_aggregator/main/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+from requests import get 
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup, Comment
+
+# Create your views here.
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200 and content_type is not None and content_type.find('html') > -1)
+
+
+def homepage(request):
+    data = None 
+
+    try: 
+        with closing(get("https://news.yahoo.com/", stream=True)) as resp:
+            if is_good_response(resp):
+                data = resp.content 
+            else: 
+                data = None
+    except RequestException as re:
+        print(re)
+        data = None
+    
+    soup = BeautifulSoup(data, 'html.parser')
+
+    tags = soup.find_all("a", class_="Fw(b)", href=True)
+
+    yahooNews = {}
+
+    for node in tags:
+        yahooNews['https://news.yahoo.com' + node['href']] = ''.join(node.find_all(text=lambda  text:not isinstance(text, Comment)))
+
+    return render(request, 'index.html', {
+        'yahooNews': yahooNews,  
+    })
